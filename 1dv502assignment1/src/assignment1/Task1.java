154b8b899e3b86945729370419960f575f795d4e
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2021-01-24 22:29:26 +0100

    Test push

--- 1dv502assignment1/src/assignment1/Task1.java
+++ 1dv502assignment1/src/assignment1/Task1.java
@@ -2,6 +2,6 @@ package assignment1;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Testing Task 1 with added git configs?");
+        System.out.println("Testing Task 1 with added git configs all?");
     }
 }
