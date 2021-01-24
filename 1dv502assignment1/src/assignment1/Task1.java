dac705239d2e7de3c71e539358933ba66078cdb4
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2021-01-24 22:34:46 +0100

    Test to push all remotes

--- 1dv502assignment1/src/assignment1/Task1.java
+++ 1dv502assignment1/src/assignment1/Task1.java
@@ -2,6 +2,6 @@ package assignment1;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Testing Task 1 with added git configs all & origin?");
+        System.out.println("Testing Task 1 pushing it all this time...");
     }
 }
