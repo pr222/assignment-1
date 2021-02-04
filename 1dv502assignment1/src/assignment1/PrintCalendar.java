2c5b49d2bb6b7fbfd168f0be8eefda22470bacd8
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2021-02-04 17:39:04 +0100

    Complete PrintCalendar class

--- 1dv502assignment1/src/assignment1/PrintCalendar.java
+++ 1dv502assignment1/src/assignment1/PrintCalendar.java
@@ -3,6 +3,11 @@ package assignment1;
 import java.util.Scanner;
 
 public class PrintCalendar {
+    /**
+     * Convert a number of a month to corresponding string representation.
+     * @param month
+     * @return
+     */
     public static String monthToString(int month) {
         String monthAsString = "";
 
@@ -35,6 +40,9 @@ public class PrintCalendar {
         return monthAsString;
     }
 
+    /**
+     * Determine wether a year is a leap year or not.
+     */
     public static boolean isLeapYear(int year) {
         if (year % 4 == 0) {
             // Check for century.
@@ -52,6 +60,12 @@ public class PrintCalendar {
         } 
     }
 
+    /**
+     * Determine the number of days in any month.
+     * @param year
+     * @param month
+     * @return
+     */
     public static int nrOfDaysInMonth(int year, int month) {
         int nrOfDays = 0;
 
@@ -61,13 +75,75 @@ public class PrintCalendar {
             } else {
                 nrOfDays = 28;
             }
-        } else if (month % 2 == 0) {
+        } else if (month <= 6 && month % 2 == 0 || month >= 9 && month % 2 != 0 ) {
             nrOfDays = 30;
         } else {
             nrOfDays = 31;
         }
 
         return nrOfDays;
+    }
+
+    /**
+     * Adjusts weekdays to start from Monday instead of Saturday with values of 0-6.
+     * @param weekday
+     * @return
+     */
+    public static int adjustWeekday(int weekday) {
+        int adjusted;
+
+        if (weekday == 0) {
+            adjusted = 6;
+        } else if (weekday == 1) {
+            adjusted = 7;
+        } else {
+            adjusted = weekday - 1;
+        }
+
+        return adjusted;
+    }
+
+    /**
+     * Format days with their numbers in a grid.
+     */
+    public static void printFormattedDays(int days, int firstMonthDay) {
+        int counter = days + 1;
+        int day = 1;
+
+        int i = 0;
+
+        while (counter > 1) {
+            if (i == 0) {
+                for (int j = 0; j < 7; j++) {
+                    if (j < firstMonthDay - 1) {
+                        System.out.print("    ");
+                    } else {
+                        if (counter > 1) {
+                            if (day < 10) {
+                                System.out.print(day + "   ");
+                            } else {
+                                System.out.print(day + "  ");
+                            }
+                            day ++;
+                            counter --;
+                        }
+                    }
+                }
+            }
+            System.out.println();
+            for (int j = 0; j < 7; j++) {
+                if (counter > 1) {
+                    if (day < 10) {
+                        System.out.print(day + "   ");
+                    } else {
+                        System.out.print(day + "  ");
+                    }
+                    day ++;
+                    counter --;
+                }
+            } 
+            i ++;
+        }
     }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -80,20 +156,18 @@ public class PrintCalendar {
 
         String monthStr = monthToString(month);
 
-        int firstWeekday = DayOfWeek.findWeekday(year, month, 1);
-        String weekday = DayOfWeek.weekdayToString(firstWeekday);
-        System.out.println(weekday); // OUT
+        int firstMonthDay = DayOfWeek.findWeekday(year, month, 1);
+        firstMonthDay = adjustWeekday(firstMonthDay);
 
-        // How many days in a month
         int days = nrOfDaysInMonth(year, month);
-
-        System.out.println("Days of month: " + days); // OUT
-
-        // Format days
 
         System.out.println(monthStr + " " + year);
         System.out.println("---------------------------");
         System.out.println("Mon Tue Wed Thu Fri Sat Sun");
+
+        printFormattedDays(days, firstMonthDay);
+        System.out.println();
+
         scan.close();
     }
 }
